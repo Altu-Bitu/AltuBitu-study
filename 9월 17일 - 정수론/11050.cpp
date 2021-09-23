28cc6e6c3548b2b8c1c6d44b6705ed67740da0d9
EunWoo Lee <rabbitew@naver.com>
2021-09-23 23:23:58 +0900

    [정수론] 9월 23일 - Update

--- 9월 17일 - 정수론/11050.cpp
+++ 9월 17일 - 정수론/11050.cpp
@@ -5,9 +5,8 @@ int factorial(int n){
     int result=1;
     if (n==1 || n==0){
         return result;
-    }else{
-        return n*factorial(n-1);
     }
+    return n*factorial(n-1);
 }
 
 int main(){
